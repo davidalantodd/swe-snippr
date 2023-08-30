ff864ba8f9f300ee157f42e499dbb64aaf7aded9
davidalantodd <david.todd@multiverse.io>
2023-08-30 14:02:19 -0400

    add edit and delete routes

--- routes/snippet.js
+++ routes/snippet.js
@@ -2,7 +2,13 @@ const route = require('express').Router()
 const { encrypt, decrypt } = require('../utils/encrypt')
 
 // array to store snippets
-const snippets = require('./seedData.json')
+const snippets = require('../seedData.json')
+
+//encrypt each snippet's code
+snippets.map((snippet, index) => {
+  // snippet.code = encrypt(snippet.code) (another way to accomplish the same thing!)
+  snippets[index] = { ...snippet, code: encrypt(snippet.code) }
+})
 
 // generate a unique ID for each snippet
 let id = snippets.length + 1
@@ -39,20 +45,35 @@ route.post('/', (req, res) => {
 route.get('/', (req, res) => {
   const { lang } = req.query
 
+  console.log("encrypted code snippets that are stored", snippets)
+
   // decrypt all snippets
-  const decodedSnippets = snippets.map(snippet => ({
+  const decodedSnippets = snippets.map(snippet => {
+    console.log(snippet)
+    return ({
     ...snippet,
     code: decrypt(snippet.code)
-  }))
+  });
+})
 
   // handle query strings
-  if (lang) {
+  // if (lang) {
+  //   const filteredSnippets = decodedSnippets.filter(
+  //     snippet => snippet.language.toLowerCase() === lang.toLowerCase()
+  //   )
+  //   return res.json(filteredSnippets)
+  // }
+
+   //filter more than exact match
+   if (lang) {
+    const regex = new RegExp(lang, "gi")
     const filteredSnippets = decodedSnippets.filter(
-      snippet => snippet.language.toLowerCase() === lang.toLowerCase()
+      snippet => snippet.language.match(regex)
     )
     return res.json(filteredSnippets)
-  }
+    }
 
+  console.log("decrypted code from get request that will be sent as a response: ", decodedSnippets)
   res.json(decodedSnippets)
 })
 
@@ -63,13 +84,65 @@ route.get('/:id', (req, res) => {
   const snippetId = parseInt(req.params.id)
   const snippet = snippets.find(snippet => snippet.id === snippetId)
 
+  console.log("encrypted code snippet that is stored", snippet)
+
   if (!snippet) {
     return res.status(404).json({ error: 'Snippet not found' })
+    
   }
 
   // decrypt before sending back
   snippet.code = decrypt(snippet.code)
+ 
+  console.log("decrypted code from get request that will be sent as a response: ", snippet)
+
   res.json(snippet)
+})
+
+
+//STRETCH GOALS
+
+// edit a snippet by ID
+route.put('/:id', (req, res) => {
+  const snippetId = parseInt(req.params.id)
+  const {language, code} = req.body
+  let foundIndex = -1
+  snippets.map((s, index) => {
+    if (s.id == snippetId){
+      foundIndex = index
+    } 
+    return s
+  })
+  if (foundIndex == -1){
+    res.status(404).json("error: 'Snippet not found, not able to be updated")
+  } else {
+    snippets.splice(foundIndex, 1, {
+      ...snippets[foundIndex],
+      "code": encrypt(code)
+    })
+  }
+  console.log(snippets)
+  res.status(200).json(`Snippet with id=${snippetId} updated to {language: ${snippets[foundIndex].language}, code: ${snippets[foundIndex].code}}`)
+})
+
+//delete a snippet by ID
+route.delete('/:id', (req, res) => {
+  const snippetId = parseInt(req.params.id)
+  const {language, code} = req.body
+  let foundIndex = -1
+  snippets.map((s, index) => {
+    if (s.id == snippetId){
+      foundIndex = index
+    } 
+    return s
+  })
+  if (foundIndex == -1){
+    res.status(404).json("error: 'Snippet not found, not able to be deleted")
+  } else{
+    snippets.splice(foundIndex, 1)
+    console.log(snippets)
+  }
+  res.status(200).json(`Snippet with id=${snippetId} deleted`)
 })
 
 module.exports = route
--- routes/user.js
+++ routes/user.js
@@ -13,9 +13,24 @@ route.post('/', basicAuth, async (req, res) => {
   const { email, password } = req.user
   const id = users.length + 1
 
+  /* To test this, we want to create an authorization header, we can do this through
+    echo -n 'test@user.com:password123' | base64
+      this will give us 'dGVzdEB1c2VyLmNvbTpwYXNzd29yZDEyMw=='
+    echo -n 'secondtime@user.com:password123456' | base64
+      c2Vjb25kdGltZUB1c2VyLmNvbTpwYXNzd29yZDEyMzQ1Ng==
+  */
+
+
   // hash the password
   const saltRounds = 10
   const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+  console.log("hashed", hashedPassword)
+  console.log("password", password)
+/* example - returns:
+  hashed $2b$10$lHmiXGV3CBglqRv9ZDy/cuL.zS8IoDMEa4B.fejbPAef3JqSLtQru
+  password password123456
+*/
 
   const user = {
     id,
